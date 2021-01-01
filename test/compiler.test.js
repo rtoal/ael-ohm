7e2f7f59f65e906a287b2bc8a1a2e2b5dddbfb50
Ray Toal <rtoal@lmu.edu>
2021-01-01 13:04:24 -0800

    Flesh out compiler tests

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,17 +1,47 @@
 import assert from "assert"
-import { LiteralExpression, PrintStatement, Program } from "../ast.js"
+import util from "util"
 import compile from "../compiler.js"
+
+const sampleProgram = "print 0"
 
 describe("The compiler", () => {
   it("says 'Unknown output type' when the output type is unknown", done => {
     assert.strictEqual(compile("print 0", "blah"), "Unknown output type")
     done()
   })
-  it("returns the expected AST for the simplest program", done => {
-    assert.deepStrictEqual(
-      compile("print 0", "ast"),
-      new Program([new PrintStatement(new LiteralExpression(0))])
-    )
+  it("calls the compiler with the ast option", done => {
+    const compiled = compile(sampleProgram, "ast")
+    assert(util.format(compiled).startsWith("   1 | program: Program"))
+    done()
+  })
+  it("calls the compiler with the analyzed option", done => {
+    const compiled = compile(sampleProgram, "analyzed")
+    assert(util.format(compiled).startsWith("   1 | program: Program"))
+    done()
+  })
+  it("calls the compiler with the optimized option", done => {
+    const compiled = compile(sampleProgram, "optimized")
+    assert(util.format(compiled).startsWith("   1 | program: Program"))
+    done()
+  })
+  it("generates js code when given the js option", done => {
+    const compiled = compile(sampleProgram, "js")
+    assert(util.format(compiled).startsWith("console.log(0)"))
+    done()
+  })
+  it("generates js code when given the js option", done => {
+    const compiled = compile(sampleProgram, "c")
+    assert(util.format(compiled).startsWith("#include"))
+    done()
+  })
+  it("generates js code when given the js option", done => {
+    const compiled = compile(sampleProgram, "llvm")
+    assert(util.format(compiled).startsWith("@format ="))
+    done()
+  })
+  it("returns an error message on a bad option argument", done => {
+    const compiled = compile(sampleProgram, "llvmmmmmmm")
+    assert(util.format(compiled).startsWith("Unknown output type"))
     done()
   })
 })
