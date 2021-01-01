12137b3ab814a85360a30fb07b96c311f121d220
Ray Toal <rtoal@lmu.edu>
2020-12-31 23:26:10 -0800

    C generator WIP

--- generator/c.js
+++ generator/c.js
@@ -1,66 +1,66 @@
-// """Code Generator Ael -> C
-
+// Code Generator Ael -> C
+//
 // Invoke generate(program) with the program node to get back the C translation
 // as a string.
-// """
 
 export default function generate(program) {
-  return null
+  const lines = []
+  let targetNames = new Map()
+  let nextSuffix = 1
+
+  function targetName(declaration) {
+    if (!targetNames.has(declaration)) {
+      targetNames.set(declaration, nextSuffix)
+      nextSuffix += 1
+    }
+    return `${declaration.name}_${targetNames.get(declaration)}`
+  }
+
+  function gen(node) {
+    return generators[node.constructor.name](node)
+  }
+
+  function emit(line) {
+    lines.push(line)
+  }
+
+  const generators = {
+    Program(self) {
+      emit("#include <stdio.h>")
+      emit("#include <math.h>")
+      emit("int main() {")
+      for (const s of self.statements) {
+        gen(s)
+      }
+      emit("return 0;")
+      emit("}")
+    },
+    Declaration(self) {
+      emit(`double ${targetName(self)} = ${gen(self.initializer)};`)
+    },
+    Assignment(self) {
+      self.source = gen(self.source)
+      self.target = gen(self.target)
+      emit(`${self.target} = ${self.source};`)
+    },
+    PrintStatement(self) {
+      emit(`printf("%g\\n", {gen(self.expression)});`)
+    },
+    BinaryExpression(self) {
+      return `(${gen(self.left)} ${self.op} ${gen(self.right)})`
+    },
+    UnaryExpression(self) {
+      const op = { abs: "fabs" }[self.op] ?? self.op
+      return `${op}(${gen(self.operand)})`
+    },
+    IdentifierExpression(self) {
+      return targetName(self.ref)
+    },
+    LiteralExpression(self) {
+      return self.value
+    },
+  }
+
+  gen(program)
+  return lines.join("\n")
 }
-
-// from io import StringIO
-// from ast import *
-
-// def generate(program):
-//     buffer = StringIO()
-//     target_names = {}
-//     next_suffix = 1
-
-//     def target_name(declaration):
-//         nonlocal next_suffix
-//         if declaration not in target_names:
-//             target_names[declaration] = next_suffix
-//             next_suffix += 1
-//         return f"{declaration.name}_{target_names[declaration]}"
-
-//     def generate(node):
-//         def emit(line):
-//             print(line, file=buffer)
-
-//         def generateProgram(self):
-//             emit("#include <stdio.h>")
-//             emit("#include <math.h>")
-//             emit("int main() {")
-//             for s in self.statements:
-//                 generate(s)
-//             emit("return 0;")
-//             emit("}")
-
-//         def generateDeclaration(self):
-//             emit(f"double {target_name(self)} = {generate(self.initializer)};")
-
-//         def generateAssignment(self):
-//             source = generate(self.source)
-//             target = generate(self.target)
-//             emit(f"{target} = {source};")
-
-//         def generatePrintStatement(self):
-//             emit(f'printf("%g\\n", {generate(self.expression)});')
-
-//         def generateBinaryExpression(self):
-//             return f"({generate(self.left)} {self.op} {generate(self.right)})"
-
-//         def generateUnaryExpression(self):
-//             op = {'abs': 'fabs'}.get(self.op, self.op)
-//             return f"{op}({generate(self.operand)})"
-
-//         def generateIdentifierExpression(self):
-//             return target_name(self.ref)
-
-//         def generateLiteralExpression(self):
-//             return self.value
-
-//         return locals()[f"generate{type(node).__name__}"](node)
-
-//     generate(program)
-//     return buffer.getvalue()
