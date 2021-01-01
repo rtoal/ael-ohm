67f5da710c3aaadd1feb3bc27abe6bb05733c983
Ray Toal <rtoal@lmu.edu>
2021-01-01 11:55:39 -0800

    Tweak test to improve coverage in code generators

--- test/generator.test.js
+++ test/generator.test.js
@@ -14,13 +14,13 @@ function dedent(s) {
 const smallFixture = {
   name: "small",
   source: `
-    let x = 3
+    let x = 3.1
     x = 5 * sqrt x / x + x - abs x
     print x
   `,
   expected: {
     js: dedent`
-      let x_1 = 3;
+      let x_1 = 3.1;
       x_1 = ((((5 * Math.sqrt(x_1)) / x_1) + x_1) - Math.abs(x_1));
       console.log(x_1);
     `,
@@ -28,7 +28,7 @@ const smallFixture = {
       #include <stdio.h>
       #include <math.h>
       int main() {
-      double x_1 = 3;
+      double x_1 = 3.1;
       x_1 = ((((5 * sqrt(x_1)) / x_1) + x_1) - fabs(x_1));
       printf("%g\\n", x_1);
       return 0;
@@ -41,11 +41,11 @@ const smallFixture = {
       declare double @llvm.sqrt.f64(double)
       define i64 @main() {
       entry:
-      %0 = call double @llvm.sqrt.f64(double 3.0)
+      %0 = call double @llvm.sqrt.f64(double 3.1)
       %1 = fmul double 5.0, %0
-      %2 = fdiv double %1, 3.0
-      %3 = fadd double %2, 3.0
-      %4 = call double @llvm.fabs(double 3.0)
+      %2 = fdiv double %1, 3.1
+      %3 = fadd double %2, 3.1
+      %4 = call double @llvm.fabs(double 3.1)
       %5 = fsub double %3, %4
       call i64 (i8*, ...) @printf(i8* getelementptr inbounds ([3 x i8], [3 x i8]* @format, i64 0, i64 0), double %5)
       ret i64 0
