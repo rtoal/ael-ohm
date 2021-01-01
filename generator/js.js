24c1c6c635be0f8222636c56cf59d766ce67a803
Ray Toal <rtoal@lmu.edu>
2021-01-01 13:16:07 -0800

    Localize the target suffix mappings in the JS and C generators

--- generator/js.js
+++ generator/js.js
@@ -5,17 +5,19 @@
 
 export default function generate(program) {
   const output = []
-  let targetNames = new Map()
 
-  // Variable names in JavaScript will be suffixed with _1, _2, _3, etc. This
-  // is because "while", for example, is a legal variable name in Ael, but not
-  // in JavaScript. So we want to generate something like "while_1".
-  function targetName(declaration) {
-    if (!targetNames.has(declaration)) {
-      targetNames.set(declaration, targetNames.size + 1)
+  // Variable names in JS will be suffixed with _1, _2, _3, etc. This is
+  // because "while", for example, is a legal variable name in Ael, but
+  // not in JS. So we want to generate something like "while_1". We handle
+  // this by mapping each variable declaration to its suffix.
+  const targetName = (mapping => {
+    return declaration => {
+      if (!mapping.has(declaration)) {
+        mapping.set(declaration, mapping.size + 1)
+      }
+      return `${declaration.name}_${mapping.get(declaration)}`
     }
-    return `${declaration.name}_${targetNames.get(declaration)}`
-  }
+  })(new Map())
 
   const gen = node => generators[node.constructor.name](node)
 
